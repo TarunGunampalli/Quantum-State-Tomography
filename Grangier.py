5af365d339398b06614ac6f99914fe663803fe92
Tarun Gunampalli <tarun.gunampalli@gmail.com>
2021-07-05 22:48:47 -0500

    ^^

--- Grangier.py
+++ Grangier.py
@@ -12,51 +12,78 @@ def find(name, path):
             return os.path.join(root, name)
 
 
+path = 'C:\\Users\\tarun\\Downloads\\'
+folderName = "runs"
+numTrials = 5
+
+fullPath = path + folderName
+
 wb = Workbook()
 
 ws = wb.active
-
-path = 'C:\\Users\\tarun\\Downloads\\'
-
-folderName = "runs"
-
-fullPath = path + folderName
 
 runs = os.listdir(fullPath)
 
 row = 2
 
+
+strengths = {}
+
 for run in runs:
     col = 1
     configFile = find("config.txt", fullPath + "\\" + run)
     detFile = find("det.txt", fullPath + "\\" + run)
+
     with open(configFile, "r") as file:
         text = file.read()
         regex = re.compile("strength=.+?(?=,)")
         result = regex.search(text)
-        strength = text[result.span()[0]+9: result.span()[1]]
+        strength = float(text[result.span()[0]+9: result.span()[1]])
+        if strength not in strengths:
+            strengths[strength] = {}
 
     with open(detFile, "r") as file:
-        detections = {}
+        detections = strengths[strength]
         lines = file.readlines()
         for line in lines:
             data = line.split(":")
+            detector = data[0]
+            if detector not in detections:
+                detections[detector] = []
             numDetections = data[1].strip()
-            detections[data[0]] = numDetections
+            detections[detector].append(int(numDetections))
 
-        ws[get_column_letter(col) + str(row)] = float(strength)
-    for detector in detections:
-        col += 1
-        ws[get_column_letter(col) + str(row)] = int(detections[detector])
+    #     ws[get_column_letter(col) + str(row)] = strength
 
-    row += 1
+    # for detector in detections:
+    #     col += 1
+    #     dataLocation = get_column_letter(col) + str(row)
+    #     numDetections = detections[detector]
+    #     ws[dataLocation] = float(sum(numDetections) / len(numDetections))
+
+    # row += 1
+
+# print(strengths)
 
 ws['A1'] = "Strength"
 col = 1
-row = 1
-for detector in detections:
+for detector in strengths[0.0]:
     col += 1
-    ws[get_column_letter(col) + str(row)] = detector
+    ws[get_column_letter(col) + '1'] = detector
+
+
+row = 2
+for strength in strengths:
+    col = 1
+    ws[get_column_letter(col) + str(row)] = strength
+    allDetectors = strengths[strength]
+    for detector in allDetectors:
+        col += 1
+        dataLocation = get_column_letter(col) + str(row)
+        detections = allDetectors[detector]
+        ws[dataLocation] = float(sum(detections) / len(detections))
+
+    row += 1
 
 wb.save(filename="Results.xlsx")
 wb.close()
